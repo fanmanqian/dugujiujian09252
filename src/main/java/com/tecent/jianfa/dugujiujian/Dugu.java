0a0552f196f5b9ac6af72d5b0edcbbb62b5e8c6b
fanmanqian <2563965485@qq.com>
2021-06-16 19:44:48 +0800

    anou

--- src/main/java/com/tecent/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/tecent/jianfa/dugujiujian/Dugu.java
@@ -3,5 +3,6 @@ package com.tecent.jianfa.dugujiujian;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("AAAAAAA");
+        System.out.println("BBBBBB");
     }
 }
